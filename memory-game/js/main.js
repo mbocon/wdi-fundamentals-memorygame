c78c0f58521acdbe04d39838edd4c814f90fe85a
Michael Bocon <mikebocon@gmail.com>
2020-01-14 19:46:28 -1000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,22 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardsInPlay);
+
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert ('You found a match!');
+	} else {
+		alert ('Sorry, try again');
+	}
+}
